59c99e593e0e3c9ca86f2d6a4d9b0c83ac1e6e1d
said7388 <mdabusaidriyaz@gmail.com>
2022-11-24 21:54:36 +0600

    update amount input

--- src/Components/EnterAmount.js
+++ src/Components/EnterAmount.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { useForm } from 'react-hook-form';
 import { useIntl } from 'react-intl';
+import { NumericFormat } from 'react-number-format';
 import { useNavigate } from 'react-router-dom';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import BodyLayout from '../Helper/BodyLayout';
@@ -17,7 +17,6 @@ const EnterAmount = () => {
   const { loginTime, personalInfoTime, destinationEnterTime, clientName, dateTimeEnterTime } = useRecoilValue(clientDataState);
   const navigate = useNavigate();
   const intl = useIntl();
-  const { register, formState: { errors }, handleSubmit } = useForm();
 
 
   const timeFormatter = (time) => (
@@ -32,27 +31,15 @@ const EnterAmount = () => {
     }))
   )
 
-  const currencyConverter = (currency) => {
-    const bdtFormat = intl.formatNumber(currency, { style: 'currency', currency: 'BDT' });
-    const dd = bdtFormat.slice(0, bdtFormat.length - 4).replace(',', '.');
-    return dd;
-  }
-
-
-  const onChangeAmount = (e) => {
-    console.log((e.target.value).replace(',', ''))
-    setAmountValue(e.target.value.replace(',', ''))
-  }
-
-  const onSubmit = (data) => {
-    console.log(data)
+  const onNextClick = () => {
+    console.log(amountValue)
 
     const time = new Date().toISOString();
 
     setClientDataState((prev) => {
       const newValue = JSON.parse(JSON.stringify(prev));
       newValue.amountEnterTime = time;
-      newValue.clientAmount = data.amount;
+      newValue.clientAmount = amountValue;
       return newValue;
     })
 
@@ -99,27 +86,34 @@ const EnterAmount = () => {
         disabled
       />
 
-      <form>
-        <Input
-          register={register}
-          required={true}
-          errors={errors}
-          name="amount"
-          title="Enter Amount"
-          onChange={onChangeAmount}
-          type="number"
-          value={currencyConverter(amountValue)}
-          preInput='৳'
+      <div className="relative">
+        <h3
+          className="text-left py-2 text-base font-medium uppercase"
+        >
+          Enter Amount (BDT)
+        </h3>
+        <span className="absolute top-11 text-xl font-medium left-2">৳</span>
+        <NumericFormat
+          defaultValue="0"
+          className='py-1.5 rounded-md px-5 text-right w-full'
+          allowLeadingZeros={false}
+          decimalScale={2}
+          fixedDecimalScale={2}
+          allowNegative={false}
+          maxLength={13}
+          thousandSeparator=","
+          onValueChange={(values) => {
+            setAmountValue(values.floatValue);
+          }}
         />
+      </div>
 
-        {errors.amount?.type === 'required' && <p role="alert" className="text-red-500">Amount must be required</p>}
 
 
-        <div className="w-full flex justify-between mt-8">
-          <Button onClick={() => navigate('/date-time-select')} title="Back" />
-          <Button onClick={handleSubmit(onSubmit)} title="Next" />
-        </div>
-      </form>
+      <div className="w-full flex justify-between mt-8">
+        <Button onClick={() => navigate('/date-time-select')} title="Back" />
+        <Button onClick={onNextClick} title="Next" />
+      </div>
     </BodyLayout>
   );
 };
